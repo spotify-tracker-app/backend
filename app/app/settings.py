e85db7a48421e778383f8d6a932308df78908289
jacekloboda <jacekloboda55@gmail.com>
2026-02-24 13:04:05 +0100

    removed dotenv module and allowed all hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +26,7 @@ SECRET_KEY = 'django-insecure-fpst1bn@qiml-!fh*7$^9t!l#-*7uxgura^ts0j)-8p9@u5nac
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
