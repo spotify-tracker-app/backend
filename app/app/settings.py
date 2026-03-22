ffdbfa7360ba37d6291d35fe8364c7ff3ccd81c9
jacekloboda <jacekloboda55@gmail.com>
2026-03-22 10:36:02 +0100

    removed secret

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fpst1bn@qiml-!fh*7$^9t!l#-*7uxgura^ts0j)-8p9@u5nac'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
